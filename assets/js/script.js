f960d9230017e0849dba43116b9dc9c57f895e8b
AsaadMBaz <asaad_dev@MacBook-Pro-AsaadMBaz.local>
2024-03-22 18:15:37 +0500

    first project

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+  $(function(){
+    $( "#inputCheckIn" ).datepicker();
+    $( "#inputCheckOut" ).datepicker();
+
+
+    $('.level-slider').slick({
+      dots: true,
+      slidesToShow: 1,
+      slideToScroll: 1,
+      arrows: false,
+     
+    });
+
+    function videoToggle()
+    {
+      video= $('.level-video').get(0)
+
+      if(video.paused)
+      {
+        video.play();
+        $('.video-control-play').hide();
+        $('.video-control-pause').show();
+      }
+      else
+      {
+        video.pause();
+        $('.video-control-play').show();
+        $('.video-control-pause').hide();
+      }
+    }
+
+    $('.video-control-play').click(function(){
+    videoToggle();
+  })
+      videoToggle();
+
+
+    $('.video-control-pause').click(function(){
+    videoToggle();
+  })
+      videoToggle();
+    
+
+
+  } );
+ 
